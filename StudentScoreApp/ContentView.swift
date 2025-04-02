5a0344ed07020073b6caef94d86bac4398527df1
HanJW8410 <158255972+HanJW8410@users.noreply.github.com>
2025-04-03 02:09:23 +0900

    Initial Commit

---
+++ StudentScoreApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StudentScoreApp
+//
+//  Created by 한지웅 on 4/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StudentScoreApp/StudentScoreAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudentScoreAppApp.swift
+//  StudentScoreApp
+//
+//  Created by 한지웅 on 4/3/25.
+//
+
+import SwiftUI
+
+@main
+struct StudentScoreAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
